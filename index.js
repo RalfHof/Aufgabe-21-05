e47881483def278cd254c5e66addc969f1f94dcf
RalfHof <164879122+RalfHof@users.noreply.github.com>
2024-05-21 15:42:37 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+let name = "Ralf"
+
+
+const root = reactDOM.createRoot(document.getElementById('root');)
+root.render(
+    <h1>hallo {name }</h1>
+  <p>hallo {name}</p>
+
+
+);
